8acfed6729ceefb8fb1df3f5dd3e2e1224cb136a
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-01-31 16:15:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';  
+
+// /* Add NavLink to importer */
+// //import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+ 
+// /* Add basic styling for NavLinks */
+ 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Message from  './Message'
+import Navbar from './Navbar' 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+ 
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} /> 
+      <Route exact path="/message" component={Message} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
